0883818cc2f1a3fee9379d5ef796633e1d1201a0
Juan Bautista Delorme Pinedo <jdelorme@c2r10s4.42madrid.com>
2024-02-02 17:17:14 +0100

    added libft

---
+++ libft/ft_strdup.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 20:19:23 by jdelorme          #+#    #+#             */
+/*   Updated: 2024/02/01 15:43:43 by jdelorme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char	*cpy;
+	int		i;
+
+	i = 0;
+	cpy = malloc((ft_strlen((char *)str) + 1) * sizeof(char));
+	if (cpy == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		cpy[i] = str[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
+/*
+int	main()
+{
+	char *ptr = NULL;
+	char *f;
+
+	f = ft_strdup(ptr);
+	printf("La copia devuelve %s \n", f);
+	return(0);
+}*/
